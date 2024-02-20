214808ad6951845d1545906e03396e382da3f197
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-20 11:00:14 +0100

    bootstrap added

--- appli/index.php
+++ appli/index.php
@@ -3,36 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styleIndx.css">
     <title>Ajout produit</title>
 </head>
 <body>
 
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du produit : 
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit : 
-                <input type="number" step="any" name = "price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée : 
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
+    <div class="container">
+        <div class="container-btn">
+            <a class="btn btn-primary" href="./index.php"><span class="glyphicon"></span>Ajouter produit</a>
+            <a class="btn btn-light" href="./recap.php"><span class="glyphicon"></span>Panier</a>
+        </div>
+        
+        <form class="form" action="traitement.php" method="post">
+            <p>
+                <label for="name">
+                    Nom du produit : 
+                    <input type="text" name="name" class="form-control" placeholder="Nom">
+                    <span class="help-inline"><?php echo $nameErrore?></span>
+                </label>
+            </p>
+            <p>
+                <label for="price">
+                    Prix du produit : 
+                    <input type="number" step="any" name = "price" class="form-control" placeholder="Prix">
+                    <span class="help-inline"><?php echo $priceErrore?></span>
+                </label>
+            </p>
+            <p>
+                <label for="quantite">
+                    Quantité désirée : 
+                    <input type="number" name="qtt" value="1" class="form-control">
+                    <span class = "help-inline"><?php echo $quantiteErrore ?></span>
+                </label>
+            </p>
+            <p>
+                <input class="btn btn-success" type="submit" name="submit" value="Ajouter le produit">
+            </p>
+        </form>
+    </div>
 
     
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 
--- appli/recap.php
+++ appli/recap.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Récapitulatif des produits</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
         if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
             echo "<p>Aucun produit en session ...</p>";
         }else{
-            echo "<table>",
+            echo "<table class="table table-striped table-bordered">",
                     "<thead>",
                         "<tr>",
                             "<th>#</th>",
@@ -42,6 +43,9 @@
         "</tbody>",
         "</table>";
         }           
-   ?>          
+   ?>  
+   
+   
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>    
