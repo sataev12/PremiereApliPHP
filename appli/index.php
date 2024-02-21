d889b5bef3894d04ca97d87d9a9b5134877e6ca4
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-21 16:00:44 +0100

    template file is added and recap file is adapted

--- appli/index.php
+++ appli/index.php
@@ -1,19 +1,7 @@
 <?php
     session_start();
-
-
+    ob_start();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styleIndx.css">
-    <title>Ajout produit</title>
-</head>
-<body>
 
     <div class="container">
         <div class="container-btn">
@@ -67,10 +55,9 @@
         </form>
     </div>
 
-    
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-
-
-</html>
+    <!-- Temporisation sortie appliPHP -->
+    <?php   
+    //Recuperer et effacer le contenu de la temparisation, le contenu est stocké dans la variable $content             
+    $content = ob_get_clean();
+    //Inclure le fichier
+    require_once "template.php"; ?>
--- appli/recap.php
+++ appli/recap.php
@@ -1,15 +1,8 @@
 <?php
     session_start();
+    ob_start();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./styleRecap.css">
-    <title>Récapitulatif des produits</title>
 </head>
 <body>
     <!-- Pour afficher le code de notre table-->
@@ -66,8 +59,13 @@
             }    
         ?>
     </div>     
-   
+   <!-- Button pour vider le panier -->
    <a class='btn btn-danger' href="traitement.php?action=clear">Vider le panier</a>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>    
+   <!-- Temporisation sortie appliPHP -->
+   <?php 
+    $t = "Recapitulative des produits";  
+    //Recuperer et effacer le contenu de la temparisation, le contenu est stocké dans la variable $content             
+    $content = ob_get_clean();
+    //Inclure le fichier
+    require_once "template.php"; ?>
+      
